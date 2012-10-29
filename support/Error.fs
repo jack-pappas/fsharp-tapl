4e541dd595f92624fb5be5a830e1c223e376f17f
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-10-29 16:13:27 -0400

    - Reverted some changes back to the original TAPL code now that the FSharpx.Compatibility.OCaml.Format library is available.

--- support/Error.fs
+++ support/Error.fs
@@ -4,6 +4,7 @@
 module Support.Error
 
 open System
+open FSharpx.Compatibility.OCaml.Format
 
 //
 exception Exit of int
@@ -20,40 +21,32 @@ let dummyinfo = UNKNOWN
 let createInfo f l c = FI (f, l, c)
 
 let errf f =
-    (*
-    print_flush(); 
-    open_vbox 0; 
+    print_flush ()
+    open_vbox 0
     open_hvbox 0; f(); print_cut(); close_box(); print_newline();
-    *)
-    f ()
-    Console.WriteLine ()
     raise (Exit 1)
 
 let printInfo = function
     (* In the text of the book, file positions in error messages are replaced
         with the string "Error:" *)
-    | FI(f,l,c) ->
-        printf "%s: %i.%i:" f l c
+    | FI (f, l, c) ->
+        print_string f;
+        print_string ":";
+        print_int l; print_string ".";
+        print_int c; print_string ":"
     | UNKNOWN ->
-        printf "<Unknown file and line>: "
+        print_string "<Unknown file and line>: "
 
-let errfAt fi f =
-    errf <| fun () ->
-        printInfo fi
-        printf " "
-        f ()
+let errfAt fi f = errf(fun()-> printInfo fi; print_space(); f())
 
-let err s =
-    errf <| fun () ->
-        printfn "Error: %s" s
+let err s = errf (fun()-> print_string "Error: "; print_string s; print_newline())
 
-let error fi s =
-    errfAt fi <| fun () ->
-        printfn "%s" s
+let error fi s = errfAt fi (fun()-> print_string s; print_newline())
 
 let warning s =
-    printfn "Warning: %s" s
+  print_string "Warning: "; print_string s;
+  print_newline()
 
 let warningAt fi s =
-    printInfo fi
-    printfn "Warning: %s" s
+  printInfo fi; print_string " Warning: ";
+  print_string s; print_newline()
